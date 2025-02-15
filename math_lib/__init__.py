371b87d52f89b3433a78e0f067d1b108a844d8ea
litart23 <litart23@gmail.com>
2025-02-15 01:52:22 +0100

    final

--- math_lib/__init__.py
+++ math_lib/__init__.py
@@ -1,3 +1,4 @@
-from .operations import add, subtract, multiply, divide
+from .operations import add, subtract, multiply, divide, helpm, ten, one,two,three,four,five,six,seven, eight, nine, numpym
 
-__all__ = ['add', 'subtract', 'multiply', 'divide', 'helpm', 'ten', 'one','two','three','four','five','six','seven', 'eight', 'nine', 'zero','numpym']    
+
+__all__ = ['add', 'subtract', 'multiply', 'divide', 'helpm', 'ten', 'one','two','three','four','five','six','seven', 'eight', 'nine','numpym']    
