41185b98a1146e4ffac083d993b9b35cc527e82a
litart23 <litart23@gmail.com>
2025-02-15 01:26:42 +0100

    fixed init again

--- math_lib/__init__.py
+++ math_lib/__init__.py
@@ -1,3 +1,3 @@
 from .operations import add, subtract, multiply, divide
 
-__all__ = ['add', 'subtract', 'multiply', 'divide', 'help_m', 'ten_m', 'one_m','two_m','three_m','four_m','five_m','six_m','seven_m', 'eight_m', 'nine_m', 'zero_m','numpy_m']    
+__all__ = ['add', 'subtract', 'multiply', 'divide', 'helpm', 'ten', 'one','two','three','four','five','six','seven', 'eight', 'nine', 'zero','numpym']    
